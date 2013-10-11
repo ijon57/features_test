576b74588ba810ef47e4fe33d375a6a7b4dd49bc
Anton S <ijon57@gmail.com>
2013-10-11 17:42:39 +0200

    waist size casting

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -2,4 +2,18 @@ class Feature < ActiveRecord::Base
 
   belongs_to :associated, polymorphic: true
   attr_accessible :associated_id, :associated_type, :type, :value
+
+  def self.value associated, feature_type, value_type = nil
+    feature = where(associated_id: associated, type: feature_type).first
+    value = feature.value if feature
+    if value && value_type
+      value = case value_type
+      when :integer then value.to_i
+      when :float then value.to_f
+      when :boolean then value=="true"
+      else value
+      end
+    end
+    value
+  end
 end
--- app/models/user_item.rb
+++ app/models/user_item.rb
@@ -1,7 +1,11 @@
 class UserItem < ActiveRecord::Base
   belongs_to :user
   belongs_to :item
-  has_many :waist_sizes, class_name: "WaistSize", as: :associated
+  has_many :features, as: :associated
 
   attr_accessible :user_id, :item_id, :user, :item
+
+  def waist_size
+    Feature.value(self, WaistSize, :integer)
+  end
 end
